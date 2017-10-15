60ad451976763eb1dfc982ffae17f91590c37b66
MrLHD <464774082@qq.com>
2017-10-15 21:56:19 +0800

    Initial commit

---
+++ maoyan_list.py
@@ -0,0 +1,84 @@
+from requests.exceptions import RequestException
+import requests
+from lxml import etree
+import json
+
+def get_one_page(url,headers):
+    try:
+        response = requests.get(url,headers=headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+
+
+def parse_one_page(html):
+    html = etree.HTML(html)
+    ranking = html.xpath('//dd/i/text()')
+    film_name = html.xpath('//dd/div/div/div[1]/p[1]/a/text()')
+    to_star = html.xpath('//dd/div/div/div[1]/p[2]/text()')
+    score_one = html.xpath('//dd/div/div/div[2]/p/i[1]/text()')
+    score_two = html.xpath('//dd/div/div/div[2]/p/i[2]/text()')
+    release_time = html.xpath('//dd/div/div/div[1]/p[3]/text()')
+    for ran,film,start,one,two,release in zip(ranking,film_name,to_star,score_one,score_two,release_time):
+        data = {
+            '排名':ran,
+            '电影':film,
+            '主演': start.strip(),
+            '评分': one+two,
+            '上映时间': release
+        }
+        yield  {
+            '排名':data['排名'],
+            '电影':data['电影'],
+            '评分': data['评分'],
+            '主演': data['主演'][3:],
+            '上映时间': data['上映时间'][5:],
+        }
+
+def main():
+    url = 'http://maoyan.com/board'
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
+    }
+    html = get_one_page(url,headers)
+    for items in parse_one_page(html):
+        writefile(items)
+
+def writefile(content):
+    with open('maoyan_list.txt','a',encoding='utf-8')as f:
+        f.write(json.dumps(content,ensure_ascii=False) + '\n')
+        f.close()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
